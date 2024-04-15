4cf5a1da0e230a89b83bcf5c1793c6e68e9b4419
nguyendangduy <nguyendangduy269@gmail.com>
2024-04-15 13:17:55 +0700

    chore: add db

---
+++ app/Filament/Resources/BookResource.php
@@ -0,0 +1,77 @@
+<?php
+
+namespace App\Filament\Resources;
+
+use App\Filament\Resources\BookResource\Pages;
+use App\Filament\Resources\BookResource\RelationManagers;
+use App\Models\Book;
+use Filament\Forms;
+use Filament\Forms\Form;
+use Filament\Resources\Resource;
+use Filament\Tables;
+use Filament\Tables\Table;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\SoftDeletingScope;
+
+class BookResource extends Resource
+{
+    protected static ?string $model = Book::class;
+
+    protected static ?string $navigationIcon = 'heroicon-o-book-open';
+
+    public static function form(Form $form): Form
+    {
+        return $form
+            ->schema([
+                //
+                Forms\Components\TextInput::make('name')->required(),
+                Forms\Components\TextInput::make('stock')->required(),
+                Forms\Components\TextInput::make('publish_year')->required(),
+                Forms\Components\Select::make('category_id')
+                    ->searchable()
+                    ->relationship('category', 'name'),
+                Forms\Components\Select::make('author_id')
+                    ->searchable()
+                    ->relationship('author', 'name'),
+                Forms\Components\Select::make('publisher_id')
+                    ->searchable()
+                    ->relationship('publisher', 'name'),
+            ]);
+    }
+
+    public static function table(Table $table): Table
+    {
+        return $table
+            ->columns([
+                //
+                Tables\Columns\TextColumn::make('name')->searchable(),
+            ])
+            ->filters([
+                //
+            ])
+            ->actions([
+                Tables\Actions\EditAction::make(),
+            ])
+            ->bulkActions([
+                Tables\Actions\BulkActionGroup::make([
+                    Tables\Actions\DeleteBulkAction::make(),
+                ]),
+            ]);
+    }
+
+    public static function getRelations(): array
+    {
+        return [
+            //
+        ];
+    }
+
+    public static function getPages(): array
+    {
+        return [
+            'index' => Pages\ListBooks::route('/'),
+            'create' => Pages\CreateBook::route('/create'),
+            'edit' => Pages\EditBook::route('/{record}/edit'),
+        ];
+    }
+}
---
+++ app/Models/Book.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+
+
+class Book extends Model
+{
+    use HasFactory;
+
+    public function category(): BelongsTo
+    {
+        return $this->belongsTo(Category::class);
+    }
+
+    public function author(): BelongsTo
+    {
+        return $this->belongsTo(Author::class);
+    }
+
+    public function publisher(): BelongsTo
+    {
+        return $this->belongsTo(Publisher::class);
+    }
+}
